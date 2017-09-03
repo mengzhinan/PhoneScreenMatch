4613f0230d266367aa9b121ed312cac8b07dc7b6
“duke” <136239173@qq.com>
2017-09-03 23:21:55 +0800

    update bat files

--- screenMatchDP.bat
+++ screenMatchDP.bat
@@ -1,2 +1,12 @@
-java -jar %~dp0\screenMatchDP.jar 360
+@echo off
+
+:: Call java jar file to make dimens.xml files.
+@java -jar %~dp0\screenMatchDP.jar 360
+
+@rem "If you are not want to make some dimens.xml files below: "
+@echo Be deleting files of 1365dp ...
+@rmdir /q /s .\res\values-w1365dp
+@echo Delete complete!
+
 pause
+
